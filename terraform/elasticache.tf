7aad67e440853eed81679341ca2b21e57bdc6407
kimminlo <kimminlo77@gmail.com>
2022-05-30 14:43:43 +0900

    Test: elasticache terraform code

---
+++ terraform/elasticache.tf
@@ -0,0 +1,10 @@
+resource "aws_elasticache_cluster" "teamg" {
+  cluster_id           = "cluster-teamg"
+  engine               = "redis"
+  node_type            = "cache.t3.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+	
+}
